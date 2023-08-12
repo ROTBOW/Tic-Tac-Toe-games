cbe2873322082c8b8975b4f717b82977c4aa108e
Josiah Leon <sjosiahleon@gmail.com>
2023-08-12 00:18:26 -0500

    added docustrings to the 3d ver

--- tictactoe_3d.py
+++ tictactoe_3d.py
@@ -18,6 +18,10 @@ END = '\033[0m'
 class Game:
 
     def __reset(self) -> None:
+        """
+        The function initializes a Tic-Tac-Toe game board with three levels, each containing a
+        3x3 grid of empty spaces represented by underscores, and sets the initial turn to be True, True being X's turn.
+        """
         gen_board = lambda: [["_"]*3 for _ in range(3)]
         self.level1: list[list[str]] = gen_board()
         self.level2: list[list[str]] = gen_board()
@@ -27,13 +31,28 @@ class Game:
         
         
     def vaild_ans(self, ans:str) -> list[bool|list[str|int]]:
+        """
+        The function takes a string input and checks if it is a valid answer for a game. It
+        returns a list with three elements: a boolean indicating if the answer is valid, a list of error
+        messages if the answer is invalid, and a list of the parsed answer values if the answer is
+        valid.
+        
+        :param ans: The `ans` parameter is a string that represents the user's answer. It is expected to
+        be in the format of three space-separated numbers, where each number represents the level, row,
+        and column of a spot on a board
+        :type ans: str
+        :return: The function `valid_ans` returns a list with three elements. The first element is a
+        boolean value indicating whether the answer is valid or not. The second element is a list of
+        strings, which contains any error messages or notifications about the invalidity of the answer.
+        The third element is a list of integers, which represents the parsed and validated answer.
+        """
         res: list[bool|list[str|int]] = [False, [], []]
         ans_list = ans.strip().split()
         
         if len(ans_list) != 3:
             res[1].append('Incorrect count of inputs!')  # type: ignore
         
-        get_title: Callable[[int], str] = lambda x: {0: 'Level', 1: 'Row', 2: 'Col'}[x]
+        get_title: Callable[[int], str] = lambda x: {0: 'Level', 1: 'Row', 2: 'Col'}[x] # why did this need to be a lambda? couldn't it have just been a dict?
         for idx, num in enumerate(ans_list):
             n = num.strip()
             if not n.isdigit():
@@ -55,20 +74,69 @@ class Game:
     
     
     def place_at(self, lvl: int, row: int, col: int) -> None:
+        """
+        The function takes in a level number, row number, and column number, and places a
+        token at the specified position in the corresponding level of a game.
+        
+        :param lvl: The `lvl` parameter is an integer that represents the level of the game. It is used
+        to determine which level to access from the `level` dictionary
+        :type lvl: int
+        :param row: The `row` parameter represents the row index of the 2D grid where you want to place
+        the token
+        :type row: int
+        :param col: The `col` parameter represents the column index where the token should be placed in
+        the `level` grid
+        :type col: int
+        """
         level = {0: self.level1, 1: self.level2, 2: self.level3}[lvl]
         level[row][col] = self.place_token()
        
         
     def __get_at(self, lvl: int, row: int, col: int) -> str:
+        """
+        The function __get_at returns a string value from a specific position in a nested list based on
+        the given level, row, and column.
+        
+        :param lvl: The `lvl` parameter is an integer that represents the level of the game. It is used
+        to determine which level's data to access
+        :type lvl: int
+        :param row: The `row` parameter represents the row index of the desired element in the specified
+        level of the object
+        :type row: int
+        :param col: The `col` parameter represents the column index of the element you want to retrieve
+        from the specified level, row, and column
+        :type col: int
+        :return: a string.
+        """
         level = {0: self.level1, 1: self.level2, 2: self.level3}[lvl]
         return level[row][col]
     
     
     def __vert_check(self, board: list[list[str]]) -> bool:
+        """
+        The function __vert_check checks if there is a vertical win in a given board.
+        
+        :param board: The parameter `board` is a 2-dimensional list of strings. It represents a game
+        board where each element in the list represents a row, and each character in the row represents
+        a cell on the board
+        :type board: list[list[str]]
+        :return: the result of calling the private method `__hori_check` with the transposed board as an
+        argument.
+        """
         return self.__hori_check(trans([x.copy() for x in board]))  # type: ignore
     
     
     def __hori_check(self, board: list[list[str]]) -> bool:
+        """
+        The function checks if there is a horizontal line of identical non-empty elements in a given
+        board.
+        
+        :param board: The `board` parameter is a 2-dimensional list of strings. It represents a game
+        board where each element in the list represents a row on the board. Each row is a list of
+        strings, where each string represents a cell on the board
+        :type board: list[list[str]]
+        :return: a boolean value.
+        """
         return any(map(
             lambda x: len(set(x)) == 1 and x[0] != '_',
             board
@@ -76,6 +144,15 @@ class Game:
         
         
     def __dia_check(self, board: list[list[str]]) -> bool:
+        """
+        The function checks if there is a winning diagonal line in a tic-tac-toe board.
+        
+        :param board: The "board" parameter is a 2-dimensional list of strings. It represents a
+        tic-tac-toe board, where each element in the list represents a cell on the board. The board is a
+        3x3 grid, and each cell can contain one of three values: 'X',
+        :type board: list[list[str]]
+        :return: a boolean value.
+        """
         left = list(map(
             lambda x: board[x[0]][x[1]],
             [[0, 0], [1, 1], [2, 2]]
@@ -98,6 +175,10 @@ class Game:
 
 
     def show(self) -> None:
+        """
+        The function "show" prints a representation of a game board with multiple levels.
+        It creates a 3d-ish illustration, there was an attempt.
+        """
         clear()
         print('      1  2  3')
         for level_idx, level in enumerate(self.levels):
@@ -112,6 +193,10 @@ class Game:
             
             
     def move(self) -> None:
+        """
+        The function prompts the user for their move in a game, validates the input, and updates the
+        game state accordingly.
+        """
         print(f'Your turn {self.place_token()}!')
         print('enter your move as: level row col\nUse Spaces to separate them!')
         while True:
@@ -126,6 +211,15 @@ class Game:
         
         
     def level_check(self, lvl: list[list[str]]) -> bool:
+        """
+        The function `level_check` checks if there is a winning combination in a given level of a game
+        board.
+        
+        :param lvl: The `lvl` parameter is a 2-dimensional list of strings. It represents a level or
+        grid of some sort, where each string represents a cell in the grid
+        :type lvl: list[list[str]]
+        :return: a boolean value.
+        """
         return any([
             self.__hori_check(lvl),
             self.__vert_check(lvl),
@@ -133,6 +227,11 @@ class Game:
         ])
         
     def vert_level_win(self) -> bool:
+        """
+        The function checks if there is a vertical win in a 3D tic-tac-toe game.
+        :return: a boolean value. It returns True if there is a vertical win in the game, and False
+        otherwise.
+        """
         for row in range(3):
             for col in range(3):
                 test: set[str] = set()
@@ -144,6 +243,13 @@ class Game:
         return False
     
     def dia_level_win(self) -> bool:
+        """
+        The function `dia_level_win` checks if there is a diagonal or level win in a game.
+        :return: The function `dia_level_win` returns a boolean value. It returns `True` if there is a
+        diagonal or level win in the game, and `False` otherwise.
+        
+        oh gosh, this hurts to look at. its pretty hardcoded...
+        """
         lleft: list[list[int]] = [[0, 0], [1, 1], [2, 2]] # lvl, row
         lright: list[list[int]] = [[0, 2], [1, 1], [2, 0]]
         
@@ -180,6 +286,12 @@ class Game:
         
         
     def over(self) -> bool:
+        """
+        The function checks if there is a win condition in a game by checking the levels, vertical
+        levels, and diagonal levels.
+        :return: a boolean value. It returns True if any of the conditions in the if statements are
+        true, and False otherwise.
+        """
         if any([ self.level_check(level) for level in self.levels ]):
             return True
         
@@ -193,6 +305,13 @@ class Game:
     
     
     def game_over(self) -> bool:
+        """
+        The function "game_over" checks if the game is over, prints the game over message, asks the
+        player if they want to play again, and returns True if they don't want to play again and False
+        if they do.
+        :return: The function `game_over` returns a boolean value. It returns `False` if the player
+        wants to play again and `True` if the player does not want to play again.
+        """
         self.turn = not self.turn
         print(RED+'GAME OVER'+END)
         print(f'Player {self.place_token()} won!')
@@ -207,6 +326,11 @@ class Game:
      
         
     def play(self) -> None:
+        """
+        The function "play" is a loop that checks if the game is over, shows the current state of the
+        game, and resets the game if necessary.
+        :return: The `play` method does not explicitly return anything.
+        """
         while True:
             if self.over():
                 self.show()
